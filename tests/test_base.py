f76e1f33b23582b84a45c242c0f8fd514e19f638
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-25 18:53:11 +0300

    Disable ANSI escape code stripping in test_base.py

--- tests/test_base.py
+++ tests/test_base.py
@@ -99,8 +99,8 @@ def test_usual_functions_in_REPL():
     after = buffer.getvalue()
     print(after)
     print('-------------')
-    after = re.compile(r'(?:\x1B[@-_]|\x9B)[0-?]*[ -/]*[@-~]').sub('', after.lstrip(before))
-    after = ''.join(ch for ch in after if ch >= ' ' or ch in '\n\r\t')
+    #after = re.compile(r'(?:\x1B[@-_]|\x9B)[0-?]*[ -/]*[@-~]').sub('', after.lstrip(before))
+    #after = ''.join(ch for ch in after if ch >= ' ' or ch in '\n\r\t')
     after = after.splitlines()
 
     child.sendline("exit()")
