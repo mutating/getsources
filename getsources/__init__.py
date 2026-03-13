62a36f6e673c8d70080aa81405fb12e619f1bb7c
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-03-13 16:24:34 +0300

    Add UncertaintyWithLambdasError import to __init__.py

--- getsources/__init__.py
+++ getsources/__init__.py
@@ -1,3 +1,4 @@
 from getsources.base import getsource as getsource
 from getsources.clear import getclearsource as getclearsource
 from getsources.hash import getsourcehash as getsourcehash
+from getsources.errors import UncertaintyWithLambdasError as UncertaintyWithLambdasError
