9abc132fd996aa569af8958600526a2997f2b7b1
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-03-13 16:25:34 +0300

    Restore getsourcehash import from getsources.hash

--- getsources/__init__.py
+++ getsources/__init__.py
@@ -1,4 +1,4 @@
 from getsources.base import getsource as getsource
 from getsources.clear import getclearsource as getclearsource
-from getsources.hash import getsourcehash as getsourcehash
 from getsources.errors import UncertaintyWithLambdasError as UncertaintyWithLambdasError
+from getsources.hash import getsourcehash as getsourcehash
